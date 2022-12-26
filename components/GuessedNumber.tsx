93ae2c5e31a947f66808989f513b97263f349802
raaaimund <raimund.rittnauer@gmail.com>
2022-12-26 02:29:23 +0100

    change value onInput

--- components/GuessedNumber.tsx
+++ components/GuessedNumber.tsx
@@ -13,6 +13,7 @@ export default function GuessedNumber(props: Props) {
                    className={"p-6 h-40 w-40 max-md:h-20 max-md:w-20 max-md:text-4xl text-8xl text-pink-400 text-center border-b border-pink-200 rounded-2xl hover:rounded-2xl hover:border hover:border-pink-600 focus:border focus:border-pink-600 focus:rounded-2xl drop-shadow focus:drop-shadow-2xl "}
                    value={props.value}
                    onKeyDown={onChangeEventHandler}
+                   onInput={onChangeEventHandler}
                    onChange={(sender) => console.log(sender.target.value)}
             />
         </div>
